8078376d63f0b3e07a3884b7e50e392721196444
v0ld <culet.devel@gmail.com>
2021-01-16 15:57:31 +0700

    refactor scripts. fix error for node2

--- scripts/fund-client.js
+++ scripts/fund-client.js
@@ -1,44 +1,26 @@
-// const Oracle = require('@chainlink/contracts/truffle/v0.6/Oracle')
-
-const clUtils = require('./cl-utils');
-const {Oracle} = require('@chainlink/contracts/truffle/v0.6/Oracle');
-
-const GanacheChainlinkClient = artifacts.require('GanacheChainlinkClient');
-const { LinkToken } = require('@chainlink/contracts/truffle/v0.4/LinkToken')
-
+const MyContract    = artifacts.require('MyContract')
+const { LinkToken } = require('@chainlink/contracts/truffle/v0.4/LinkToken');
 
 let env_net1 = require('dotenv').config({ path: '../build/addrs_network1.env' })
 let env_net2 = require('dotenv').config({ path: '../build/addrs_network2.env' })
 
+
 module.exports = async callback => {
-console.log(1)
-  let adr = process.argv[5] === 'network1' ? env_net1.parsed.ORACLE_CONTRACT_ADDRESS : process.argv[5] === 'network2' ? env_net2.parsed.ORACLE_CONTRACT_ADDRESS : '0x0';
-console.log(process.argv[5])
-  const getAddr = require('../'+process.argv[5]+'/chainlink/get-addr');
 
-  console.log(`========================== TRANSFER ETH TO CHAINLINK NODE ON  ${adr} ==========================`);
+  LinkToken.setProvider(web3.currentProvider);
+  
+  let link = process.argv[5] === 'network1' ? env_net1 : process.argv[5] === 'network2' ? env_net2 : '0x0';
 
-try {
-   LinkToken.setProvider(web3.currentProvider);
-   Oracle.setProvider(web3.currentProvider);
-   let oracle = await Oracle.at(adr)
-   console.log(oracle.address)
-  const accountAddr = await getAddr();
-  const accounts = await web3.eth.getAccounts();
+  console.log(`========================== TRANSFER LINKS TO ${link.parsed.CLIENT_ADDRESS} ON NETWORK ${process.argv[5]} ==========================\n`);
 
-  let adr_cl = process.argv[5] === 'network1' ? env_net1.parsed.CLIENT1_ADDRESS : process.argv[5] === 'network2' ? env_net1.parsed.CLIENT2_ADDRESS : '0x0';
-
-  const ganacheClient = await GanacheChainlinkClient.at(adr_cl);
-  const tokenAddress  = await ganacheClient.getChainlinkToken();
+  const myContract    = await MyContract.at(link.parsed.CLIENT_ADDRESS);
+  const tokenAddress  = await myContract.getChainlinkToken();
   const token         = await LinkToken.at(tokenAddress);
-  console.log(`Transfering 5 LINK to ${ganacheClient.address}...`);
-  const tx2 = await token.transfer(accountAddr, `100000000000000000000`,{from: accounts[0]});
-  console.log(`Transfer succeeded! Transaction ID: ${tx2.tx}.`);
-  const balance = await token.balanceOf(accountAddr);
-  console.log(`Balance LINK TEKEN ON CHAINLINK NODE: ${balance.toString()}.`);
-} catch(e) {
-console.log(e)
-}
+  const tx            = await token.transfer(myContract.address, `100000000000000000000`, { from: (await web3.eth.getAccounts())[0] });
+
+  console.log(`Transfer succeeded! Transaction ID: ${tx.tx}.`);
+  console.log(`Balance recepient: ${myContract.address} is: ${(await token.balanceOf(myContract.address)).toString()} LINK`);
+  
 
   callback();
 }
--- scripts/prep-node.js
+++ scripts/prep-node.js
@@ -1,56 +1,47 @@
-// const Oracle = require('@chainlink/contracts/truffle/v0.6/Oracle')
-
-const clUtils = require('./cl-utils');
-const {Oracle} = require('@chainlink/contracts/truffle/v0.6/Oracle');
-
-const GanacheChainlinkClient = artifacts.require('GanacheChainlinkClient');
+const { Oracle }    = require('@chainlink/contracts/truffle/v0.6/Oracle');
 const { LinkToken } = require('@chainlink/contracts/truffle/v0.4/LinkToken')
+const MyContract    = artifacts.require('MyContract');
 
 
 let env_net1 = require('dotenv').config({ path: '../build/addrs_network1.env' })
 let env_net2 = require('dotenv').config({ path: '../build/addrs_network2.env' })
 
 module.exports = async callback => {
-console.log(1)
-  let adr = process.argv[5] === 'network1' ? env_net1.parsed.ORACLE_CONTRACT_ADDRESS : process.argv[5] === 'network2' ? env_net2.parsed.ORACLE_CONTRACT_ADDRESS : '0x0';
-console.log(process.argv[5])
+
+  let adrOracle = process.argv[5] === 'network1' ? env_net1.parsed.ORACLE_CONTRACT_ADDRESS : process.argv[5] === 'network2' ? env_net2.parsed.ORACLE_CONTRACT_ADDRESS : '0x0';
+
   const getAddr = require('../'+process.argv[5]+'/chainlink/get-addr');
 
-  console.log(`========================== TRANSFER ETH TO CHAINLINK NODE ON  ${adr} ==========================`);
+  console.log(`========================== TRANSFER ETH/LINK ON CHAINLINK NODE ON  ${await getAddr()} ==========================\n`);
 
-try {
+
    LinkToken.setProvider(web3.currentProvider);
    Oracle.setProvider(web3.currentProvider);
-   let oracle = await Oracle.at(adr)
-   console.log(oracle.address)
+   let oracle = await Oracle.at(adrOracle)
+   
 	
-
-	console.log(4)
   const accountAddr = await getAddr();
-	console.log(5)
-  console.log(`Setting fulfill permission to true for ${accountAddr}...`);
+	console.log(`Setting fulfill permission to true for ${accountAddr} on oracle: ${oracle.address}`);
   const accounts = await web3.eth.getAccounts();
-  const tx = await oracle.setFulfillmentPermission(accountAddr, true, {from: accounts[0]});
+  const tx       = await oracle.setFulfillmentPermission(accountAddr, true, {from: accounts[0]});
   console.log(`Fulfillment succeeded! Transaction ID: ${tx.tx}.`);
 
   
-  console.log(`Sending 1 ETH from ${accounts[0]} to ${accountAddr}.`);
+  console.log(`Sending 100 ETH from ${accounts[0]} to ${accountAddr}.`);
   const result = await web3.eth.sendTransaction({from: accounts[0], to: accountAddr, value: '100000000000000000000'});
   console.log(`Transfer succeeded! Transaction ID: ${result.transactionHash}.`);
 
-  let adr_cl = process.argv[5] === 'network1' ? env_net1.parsed.CLIENT1_ADDRESS : process.argv[5] === 'network2' ? env_net1.parsed.CLIENT2_ADDRESS : '0x0';
+  let adr_cl = process.argv[5] === 'network1' ? env_net1.parsed.CLIENT_ADDRESS : process.argv[5] === 'network2' ? env_net2.parsed.CLIENT_ADDRESS : '0x0';
 
-  const ganacheClient = await GanacheChainlinkClient.at(adr_cl);
-  const tokenAddress  = await ganacheClient.getChainlinkToken();
+  const myContract    = await MyContract.at(adr_cl);
+  const tokenAddress  = await myContract.getChainlinkToken();
   const token         = await LinkToken.at(tokenAddress);
-  console.log(`Transfering 5 LINK to ${ganacheClient.address}...`);
-  const tx2 = await token.transfer(accountAddr, `100000000000000000000`,{from: accounts[0]});
+  console.log(`Transfering 100 LINK to ${myContract.address}...`);
+  const tx2           = await token.transfer(accountAddr, `100000000000000000000`,{from: accounts[0]});
   console.log(`Transfer succeeded! Transaction ID: ${tx2.tx}.`);
-  const balance = await token.balanceOf(accountAddr);
+  const balance       = await token.balanceOf(accountAddr);
   console.log(`Balance LINK TEKEN ON CHAINLINK NODE: ${balance.toString()}.`);
-} catch(e) {
-console.log(e)
-}
+
 
   callback();
 }
