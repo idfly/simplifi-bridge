b3bd8d0a5b0937ebf5289c35063fbc596d0b1d7f
v0ld <culet.devel@gmail.com>
2021-01-16 13:14:33 +0700

    delete form utils/helper.js pointing of 1/2 => CLIENT1_ACCOUNT -> CLIENT_ACCOUNT

--- utils/helper.js
+++ utils/helper.js
@@ -3,23 +3,17 @@
 const fs = require('fs');
 const path = require('path');
 
-/**
-   Add address under certain network
-*/
 const writeEnv = async (linkToken, oracle, client_address, network = 'network1') => {
 
-    let cl_address_name = 'CLIENT1_ADDRESS';
-    if(network === 'network2') cl_address_name = 'CLIENT2_ADDRESS';
-
     const addrFile = path.join(__dirname, '..', 'build', 'addrs_'+network+'.env');
+    
       try {
         fs.unlinkSync(addrFile);
       } catch {
         // delete if exists; ignore errors
       }
-
-      
-      fs.writeFileSync(addrFile, `LINK_CONTRACT_ADDRESS=${linkToken}\nORACLE_CONTRACT_ADDRESS=${oracle}\n${cl_address_name}=${client_address}\n`);
+  
+      fs.writeFileSync(addrFile, `LINK_CONTRACT_ADDRESS=${linkToken}\nORACLE_CONTRACT_ADDRESS=${oracle}\nCLIENT_ADDRESS=${client_address}\n`);
 
   }
 
