88c0db88e2d4bd4aacfeefea2764436e735cc15e
Атрощенко Александр <alekcangp@list.ru>
2021-01-19 11:09:01 +0600

    Modified the swap and liquidity tabs

--- js/script.js
+++ js/script.js
@@ -5,21 +5,45 @@ var vm = new Vue({
       buttonBsc: '...',
       accountEth: '',
       accountBsc: '',
+      //Exchange tab
       ethChainId: '',
       bscChainId: '',
       tokenFrom: '',
       tokenTo: '',
-      USDCbin: '<img src="./img/usdc.png" width="20" height="20"/>  USDC',
-      USDCeth: '<img src="./img/usdc.png" width="20" height="20"/>  USDC',
-      USDTeth: '<img src="./img/tether.png" width="20" height="20"/>  USDC',
-      USDTbin: '<img src="./img/tether.png" width="20" height="20"/>  USDC',
-      dLINKbin: '<img src="./img/dlink.png" width="20" height="20"/>  dLINK',
+      amountFrom: '',
+      amountTo:'',
+      chains: [{id:4, name:"Ethereum rinkeby", icon:"ethereum.png"}, {id:97, name:"Binance Smart Chain testnet", icon:"bsc.webp"}],
+      balanceFrom:0.01,
+      balanceTo:0.02,
+      balanceLiqEth:0.1,
+      balanceLiqBsc:0.1,
+      tokensEth: [{symbol:"USDC",addr:"",icon:"usdc.webp"},{symbol:"USDT",addr:"",icon:"tether.webp"}],
+      tokensBsc: [{symbol:"USDC",addr:"",icon:"usdc.webp"},{symbol:"USDT",addr:"",icon:"tether.webp"},{symbol:"dLINK",addr:"",icon:"dlink.webp"}],
+      itemsFrom:{},
+      itemsTo:{},
+      //Liquidity tab
+      amountLiqEth:'',
+      amountLiqBsc:'',
+      tokenLiqEth:'',
+      tokenLiqBsc:'',
       
+
     }
   })
 
-  vm.tokenFrom = vm.USDCeth;
-  vm.tokenTo = vm.USDCbin;
+//set default params
+  //for exchange
+  vm.tokenFrom = vm.tokensEth[0];
+  vm.tokenTo = vm.tokensBsc[0];
+  vm.itemsFrom = vm.tokensEth;
+  vm.itemsTo = vm.tokensBsc;
+  vm.chainFrom = vm.chains[0];
+  vm.chainTo = vm.chains[1];
+  //for liquidity
+  vm.tokenLiqEth = vm.tokensEth[0];
+  vm.tokenLiqBsc = vm.tokensBsc[0];
+  
+
 
 setTimeout(checkInstall, 500);
 
@@ -27,7 +51,33 @@ var delta =360;
   function rotate360Deg(ele){
       ele.style.webkitTransform="rotate("+delta+"deg)";
       delta+=360;
+      //rotate data
+      let chain,balance,amount,token,items;
+      chain = vm.chainTo; vm.chainTo = vm.chainFrom; vm.chainFrom = chain;
+      balance = vm.balanceTo; vm.balanceTo = vm.balanceFrom; vm.balanceFrom = balance;
+      items = vm.itemsTo; vm.itemsTo = vm.itemsFrom; vm.itemsFrom = items;
+      token = vm.tokenTo; vm.tokenTo = vm.tokenFrom; vm.tokenFrom = token;
+      amount = vm.amountTo; vm.amountTo = vm.amountFrom; vm.amountFrom = amount;
+
   }
+
+function setTokenFrom(item) {
+  vm.tokenFrom = item;
+  //getBalanceFrom()
+}
+
+function setTokenTo(item) {
+  vm.tokenTo = item;
+  //getBalanceFrom()
+}
+
+function setLiqToken(item, typ) {
+  if (typ == 'eth') {vm.tokenLiqEth = item;}
+  else {vm.tokenLiqBsc = item;}
+  
+  //getBalanceFrom()
+}
+
 
 async function checkInstall() {
     if (window.ethereum) { 
@@ -79,7 +129,7 @@ function refreshAccountDataEth() {
 // BSC wallet
 
 async function connectBsc() {
-  if (vm.buttonEth == "Install Binance") { alert(0); window.open("https://docs.binance.org/smart-chain/wallet/binance.html"); return}
+  if (vm.buttonBsc == "Install Binance") {window.open("https://docs.binance.org/smart-chain/wallet/binance.html"); return}
  //await BinanceChain.enable();
  const accountsBsc = await web3bsc.eth.getAccounts();
  vm.accountBsc = accountsBsc[0];
