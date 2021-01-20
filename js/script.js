a3aadaf1df2618fb2a94abf08530a52623a5350d
Атрощенко Александр <alekcangp@list.ru>
2021-01-20 13:34:05 +0600

    fixed big number

--- js/script.js
+++ js/script.js
@@ -11,14 +11,14 @@ var vm = new Vue({
       tokenFrom: '',
       tokenTo: '',
       amountFrom: '',
-      amountTo:'',
-      chains: [{id:4, name:"Ethereum rinkeby", icon:"ethereum.png"}, {id:97, name:"Binance Smart Chain testnet", icon:"bsc.webp"}],
+      amountTo: '',
+      chains: [{id:4, name:"Ethereum rinkeby", icon:"ethereum.png"}, {id:97, name:"BSC testnet", icon:"bsc.webp"}],
       balanceFrom:0.01,
       balanceTo:0.02,
       balanceLiqEth:0.1,
       balanceLiqBsc:0.1,
-      tokensEth: [{symbol:"USDC",addr:"",icon:"usdc.webp"},{symbol:"USDT",addr:"",icon:"tether.webp"}],
-      tokensBsc: [{symbol:"USDC",addr:"",icon:"usdc.webp"},{symbol:"USDT",addr:"",icon:"tether.webp"},{symbol:"dLINK",addr:"",icon:"dlink.webp"}],
+      tokensEth: [{symbol:"USDC",addr:"",icon:"usdc.webp",price:1},{symbol:"USDT",addr:"",icon:"tether.webp",price:1}], //price bypass
+      tokensBsc: [{symbol:"USDC",addr:"",icon:"usdc.webp",price:1},{symbol:"USDT",addr:"",icon:"tether.webp",price:1},{symbol:"dLINK",addr:"",icon:"dlink.webp",price:0.1}],
       itemsFrom:{},
       itemsTo:{},
       //Liquidity tab
@@ -26,9 +26,50 @@ var vm = new Vue({
       amountLiqBsc:'',
       tokenLiqEth:'',
       tokenLiqBsc:'',
-      
+      price: 1,
 
-    }
+    },
+    watch: {
+      
+      amountFrom: function() {
+        if (document.activeElement.id == 'num1' ) {
+        calcAmount('from')
+        }
+      }, 
+     
+      amountTo: function() {
+        if (document.activeElement.id == 'num2' ) {
+        calcAmount('to')
+        }
+      },
+      
+      tokenFrom: function() {
+        calcPrice('to'); 
+        
+      },
+      tokenTo: function() {
+        
+        calcPrice('from');
+        
+      },
+
+      buttonEth: function() {
+        if (this.buttonEth.substr(0,2) == '0x') {
+          document.getElementById("pulse2").classList.add("shadow");
+        } else {
+          document.getElementById("pulse2").classList.remove("shadow");
+        }
+      },
+
+      buttonBsc: function() {
+        if (this.buttonBsc.substr(0,2) == '0x') {
+          document.getElementById("pulse1").classList.add("shadow");
+        } else {
+          document.getElementById("pulse1").classList.remove("shadow");
+        }
+      },
+
+    } 
   })
 
 //set default params
@@ -39,11 +80,36 @@ var vm = new Vue({
   vm.itemsTo = vm.tokensBsc;
   vm.chainFrom = vm.chains[0];
   vm.chainTo = vm.chains[1];
+ 
   //for liquidity
   vm.tokenLiqEth = vm.tokensEth[0];
   vm.tokenLiqBsc = vm.tokensBsc[0];
-  
 
+  function calcPrice(ft) {
+    vm.price = vm.tokenFrom.price/vm.tokenTo.price; 
+    calcAmount(ft);
+  }
+
+  function calcAmount(ft) {
+     if (ft == 'from' && (vm.amountFrom === '' || vm.amountFrom == 0 ) ) {vm.amountTo = ''; return} 
+     if (ft == 'to' && (vm.amountTo === '' || vm.amountTo == 0) ) {vm.amountFrom = ''; return}
+     
+     if (ft == 'from') { vm.amountTo = BigNumber(vm.amountFrom).times(vm.price); } else { vm.amountFrom = BigNumber(vm.amountTo).div(vm.price)} ;
+  }
+ 
+
+
+  //input only number
+  function isNumberKey(evt,id,ft)
+  {
+    var data = document.getElementById(id).value;
+    if((evt.charCode>= 48 && evt.charCode <= 57) || evt.charCode== 46 ||evt.charCode == 0){
+    if(data.indexOf('.') > -1 || data == ''){
+     if(evt.charCode== 46)
+      evt.preventDefault();
+    }
+    }else evt.preventDefault();
+  }  
 
 setTimeout(checkInstall, 500);
 
@@ -82,13 +148,14 @@ function setLiqToken(item, typ) {
 async function checkInstall() {
     if (window.ethereum) { 
         window.web3eth = new Web3(ethereum);
-        vm.buttonEth = "Connect Ethereum";
+        vm.buttonEth = "Connect to Metamask";
     } else {vm.buttonEth = "Install Metamask"}
 
     if (window.BinanceChain) {
         window.web3bsc = new Web3(BinanceChain); 
-        vm.buttonBsc = "Connect Binance";
-    } else {vm.buttonBsc = "Install Binance"}
+        vm.buttonBsc = "Connect to Binance wallet";
+    } else {vm.buttonBsc = "Install Binance wallet"}
+
 }
 
 //Ethereum wallet
@@ -120,7 +187,7 @@ function onConnectEth() {
   }
 
 function refreshAccountDataEth() {
-  (vm.accountEth != '') ? vm.buttonEth = vm.accountEth.substr(0,6) + '...' + vm.accountEth.substr(38) : vm.buttonEth = 'Connect Ethereum'
+  (vm.accountEth != '') ? vm.buttonEth = vm.accountEth.substr(0,6) + '...' + vm.accountEth.substr(38) : vm.buttonEth = 'Connect to Metamask'
   
 
   }
@@ -129,7 +196,7 @@ function refreshAccountDataEth() {
 // BSC wallet
 
 async function connectBsc() {
-  if (vm.buttonBsc == "Install Binance") {window.open("https://docs.binance.org/smart-chain/wallet/binance.html"); return}
+  if (vm.buttonBsc == "Install Binance wallet") {window.open("https://docs.binance.org/smart-chain/wallet/binance.html"); return}
  //await BinanceChain.enable();
  const accountsBsc = await web3bsc.eth.getAccounts();
  vm.accountBsc = accountsBsc[0];
@@ -138,12 +205,12 @@ async function connectBsc() {
 
 function onConnectBsc() {
     // Subscribe to accounts change
-    BinanceChain.on("accountsChanged", (accounts) => {alert('2'+accounts);
+    BinanceChain.on("accountsChanged", (accounts) => {
       vm.accountBsc = accounts[0];
       refreshAccountDataBsc()
     });
    
-    BinanceChain.on("chainChanged", (chainId) => {alert(chainId);
+    BinanceChain.on("chainChanged", (chainId) => {
       vm.bscChainId = chainId;
       refreshAccountDataBsc();
     });
