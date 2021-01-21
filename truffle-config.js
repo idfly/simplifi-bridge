d4e5d2eccee4fc10a66a2513349a21a8e340477b
v0ld <culet.devel@gmail.com>
2021-01-21 11:05:24 +0700

    update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -16,28 +16,17 @@ module.exports = {
      network_id: "1337",
     },
 
-    live: {
-      provider: () => {
-        return new HDWalletProvider(process.env.MNEMONIC, process.env.RPC_URL)
-      },
-      network_id: '*',
-      // ~~Necessary due to https://github.com/trufflesuite/truffle/issues/1971~~
-      // Necessary due to https://github.com/trufflesuite/truffle/issues/3008
-      skipDryRun: true,
-    },
-
-    testnetBsc: {
-
-       provider: () => new HDWalletProvider(process.env.MNEMONIC_TESTNET, `https://data-seed-prebsc-1-s1.binance.org:8545/`),
+    binancetestnet: {
+       provider: () => new HDWalletProvider(process.env.MNEMONIC_TESTNET_BSC, `ws://95.217.104.54:8576`),
        network_id: 97,
        timeoutBlocks: 200  
     },
-    /*mainnetBsc: {
-
-       provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://bsc-dataseed.binance.org/`),
-       network_id: 56,
+    rinkeby: {
+       provider: () => new HDWalletProvider(process.env.MNEMONIC_TESTNET_RINKEBY, `wss://rinkeby.infura.io/ws/v3/b9e220845c084e9195c77c542a852dd7`),
+       network_id: 4,
        timeoutBlocks: 200  
-    }*/
+    }  
+    
   },
   compilers: {
     solc: {
