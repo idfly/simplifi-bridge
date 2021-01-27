798461d0dc2b9758a7f9ea02191755068e48296b
v0ld <culet.devel@gmail.com>
2021-01-27 13:59:00 +0700

    recreate sh script from Bogdan

---
+++ scripts/bash/update_env_adapter.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo  "# env for connect to ${2:-\"null\"}  
+PORT=${1:-\"null\"}
+LISTEN_NETWORK=${2:-\"null\"}
+POOL_ADDRESS=${3:-\"null\"}
+ORACLE_CONTRACT_ADDRESS=${4:-\"null\"}
+TOKENPOOL_ADDRESS=${5:-\"null\"}" > $6
