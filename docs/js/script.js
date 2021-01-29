1f5e5cecee1dba5f8f89341d8ef0b9b29ab2f0b4
Bogdan Sivochkin <syi3@ya.ru>
2021-01-29 20:45:48 +0300

    work swap need to refactor blocking buttons

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,8 +3,8 @@ var vm = new Vue({
     el: '#app',
     data: {
       chains: [{id:0x4, name:"Ethereum rinkeby", icon:"ethereum.png", web:"web3eth"}, {id:0x61, name:"BSC testnet", icon:"bsc.webp",web:"web3bsc"}],
-      tokensEth: [{symbol:"AAVE",addr:"0x918809f0c1d4c5e56328742406ddbf6bf7807c73",icon:"aave_.png",price:1}], //price bypass{symbol:"USDT",addr:"",icon:"tether.webp",price:1} {symbol:"USDC",addr:"0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b",icon:"usdc.webp",price:1}
-      tokensBsc: [{symbol:"BNB",addr:"0x55797e477BE468855690c660AA2640d3E9F80Cc6",icon:"bnb.png",price:1},{symbol:"dLINK",addr:"0x88e69c0d2d924e642965f8dd151dd2e24ba154f8",icon:"dlink.webp",price:0.1}],//{symbol:"USDC",addr:"0x64544969ed7ebf5f083679233325356ebe738930",icon:"usdc.webp",price:1}
+      tokensEth: [{symbol:"AAVE",addr:"0x918809f0c1d4c5e56328742406ddbf6bf7807c73",icon:"dai.webp",price:1}], //price bypass{symbol:"USDT",addr:"",icon:"tether.webp",price:1} {symbol:"USDC",addr:"0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b",icon:"usdc.webp",price:1}
+      tokensBsc: [{symbol:"BNB",addr:"0x55797e477BE468855690c660AA2640d3E9F80Cc6",icon:"dai.webp",price:1},{symbol:"dLINK",addr:"0x88e69c0d2d924e642965f8dd151dd2e24ba154f8",icon:"dlink.webp",price:0.1}],//{symbol:"USDC",addr:"0x64544969ed7ebf5f083679233325356ebe738930",icon:"usdc.webp",price:1}
       dexPoolETH:[{addr:"0x9f9A020ef5f14b126e2d76BD984a88a0ba9c89aA"}],
       dexPoolBSC:[{addr:"0x0B998d26B8Ab9e1caaf084Ba30ac6859Adcc236E"}],
       buttonEth: '...',
@@ -143,6 +143,7 @@ var delta =360;
       delta+=360;
       //rotate data
       console.log(vm.chainTo.id, vm.chainFrom.id)
+      console.log("dcsdccddc")
       // let exToken = vm.tokenTo;
       // setTokenTo(vm.tokenFrom)
       // setTokenFrom(vm.itemsTo)
@@ -367,12 +368,16 @@ function exchButtons(a,s,chain) {
   if (chain == 'allo'){
     if (alloEth*alloBsc > 0) { aa = 0; ss = 1}   
 } 
-  
-  if (aa == 1 && alloEth*alloBsc == 0 ) document.querySelector("#approve").removeAttribute("disabled"); else {document.querySelector("#approve").setAttribute("disabled", "disabled");}
+  console.log(`${aa} ${alloEth} ${alloBsc} ${aa == 1 && alloEth*alloBsc == 0}`)
+  if (aa == 1 || alloEth*alloBsc == 0 ) document.querySelector("#approve").removeAttribute("disabled"); else {document.querySelector("#approve").setAttribute("disabled", "disabled");}
+  console.log(`${ss} ${cc} ${ss == 1 && cc == 1}`)
   if (ss == 1 && cc == 1) document.querySelector("#swap").removeAttribute("disabled"); else document.querySelector("#swap").setAttribute("disabled", "disabled");
 
 
   }
+
+
+
 
 //APPROVE
 
@@ -382,7 +387,7 @@ function exchButtons(a,s,chain) {
     console.log(`allowanceBsc(${x})`);
     var tok,acc;
     if (vm.chainFrom.id == '0x61') {tok = vm.tokenFrom.addr; acc = vm.accountFrom;} else { tok = vm.tokenTo.addr; acc = vm.accountTo; }
-      const tokenContract = new web3bsc.eth.Contract(erc20abi, tok);
+      const tokenContract = await new web3bsc.eth.Contract(erc20abi, tok);
       if (!x) {
         await tokenContract.methods.allowance(acc,serviceContractBsc).call().then(function (res) {
         console.log("RESULT", res, "token", tok);
@@ -441,6 +446,8 @@ function exchButtons(a,s,chain) {
       let receipt = await web3eth.eth.getTransactionReceipt(tx.transactionHash);      
       if (receipt != null){
         console.log(receipt);
+        document.querySelector("#swap").setAttribute("disabled", "disabled");
+        document.querySelector("#approve").removeAttribute("disabled");
         } else {
           console.error("receipt null")
         }
@@ -448,16 +455,16 @@ function exchButtons(a,s,chain) {
 }
 
 async function getAllowanceEth(token) {
-    token.methods.allowance(vm.accountFrom, serviceContractEth).call().then(function (res) {
-        alloEth = res;
-        console.log("allowance", res, "token", token._address);
-        if (res == 0) approveTokenEth = token._address;
-        exchButtons(1,1,'eth')//
+    await token.methods.allowance(vm.accountFrom, serviceContractEth);
+       console.log("allowance", res, "token", token._address);
+        exchButtons(1,1,'eth')
         exchButtons(1,1,'allo')
+        alloBsc == 0 
+        alloEth = 0;
+        // console.log("allowance", res, "token", token._address);
+      
+       // 
 
-    }).catch(e=>{
-        console.error(e);
-    });
 
 }
 
@@ -487,6 +494,7 @@ async function setaAllowanceEth(x, tokenContract) {
     let receipt = await web3eth.eth.getTransactionReceipt(tx.transactionHash);
             if (receipt != null){
                 console.log(receipt);
+                document.querySelector("#swap").removeAttribute("disabled"); 
                 } else {
                   console.error("receipt null")
                 }
