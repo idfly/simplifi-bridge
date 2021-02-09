778ff4427a6cbacb2837bcb2eae3c3faa9903bcf
v0ld <culet.devel@gmail.com>
2021-02-09 21:14:10 +0700

    update swapDeposit script (2 variable)

--- scripts/init/5_swap_deposit.js
+++ scripts/init/5_swap_deposit.js
@@ -15,11 +15,11 @@ let recipent_overside = "0x9805E1FD9013f79b04F6cED13696F9346b70cC39"; // net1
 
       const dexPool       = await DexPool.at(addresses.POOL_ADDRESS);
       const token_pool    = await ERC20.at(addresses.TOKENPOOL_ADDRESS);
-                            //await token_pool.approve(dexPool.address, await web3.utils.toWei('16','Ether'));
+                            //await token_pool.approve(dexPool.address, await web3.utils.toWei('11','Ether'));
 
 const user = (await web3.eth.getAccounts())[0];
-console.log('11');
-      const tx            = await dexPool.swapDeposit(await web3.utils.toWei('1','Ether'), recipent_overside, {from: user});
+
+      const tx            = await dexPool.swapDeposit(await web3.utils.toWei('8','Ether'),await web3.utils.toWei('1','Ether'), recipent_overside, {from: user});
       console.log(`Network ${process.argv[5]} tx:${tx.tx}`);
 
   
