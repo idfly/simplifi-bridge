7713d6b94e414e173a65e8309d3ec33b377dc104
v0ld <culet.devel@gmail.com>
2021-02-09 22:48:57 +0700

    update script setPermissionJobId

--- scripts/init/1_set_jobId.js
+++ scripts/init/1_set_jobId.js
@@ -1,3 +1,4 @@
+const Oracle     = artifacts.require('Oracle')
 const DexPool    = artifacts.require('DexPool')
 const MyContract = artifacts.require('MyContract')
 const { LinkToken } = require('@chainlink/contracts/truffle/v0.4/LinkToken');
@@ -10,9 +11,15 @@ module.exports = async callback => {
 try{
   
       const myContract    = await MyContract.at(env.parsed.CLIENT_ADDRESS);
-      const tx            = await myContract.setJobID(await web3.utils.fromAscii("d1a27394830b43f7997e38345921323a"));    
+      const tx            = await myContract.setJobID(await web3.utils.fromAscii("d1a27394830b43f7997e38345921323a"));
       
       console.log(`>>>>>>> SET JOBID: ${myContract.address} ON NETWORK ${process.argv[5]}\ntx:${tx.tx}\n\n`);
+
+      const oracle        = await Oracle.at(env.parsed.ORACLE_CONTRACT_ADDRESS);
+            tx            = await oracle.setPermissionJobId(await web3.utils.fromAscii("d1a27394830b43f7997e38345921323a"));
+
+	   console.log(`>>>>>>> SET PermissionJobId: ${myContract.address} ON NETWORK ${process.argv[5]}\ntx:${tx.tx}\n\n`);            
+
     
   
 }catch(e){console.log(e);}
