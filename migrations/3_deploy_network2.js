f1317a1c9e5f54294ebac756121c41d5565c583e
v0ld <culet.devel@gmail.com>
2021-02-19 20:59:07 +0700

    update migration oracle

--- migrations/3_deploy_network2.js
+++ migrations/3_deploy_network2.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 
 const MyContract    = artifacts.require('MyContract')
 const { LinkToken } = require('@chainlink/contracts/truffle/v0.4/LinkToken')
-const Oracle        = artifacts.require('Oracle')
+const { Oracle }    = require('@chainlink/contracts/truffle/v0.6/Oracle')
 const DexPool       = artifacts.require('DexPool')
 const { Hexstring } = require('../lib/Hexstring')
 const DaiToken      = artifacts.require('DaiToken')
@@ -20,6 +20,7 @@ module.exports = async (deployer, network, accounts) => {
 
             LinkToken.setProvider(deployer.provider)
             Hexstring.setProvider(deployer.provider)
+            Oracle.setProvider(deployer.provider)
 
             
             try {
