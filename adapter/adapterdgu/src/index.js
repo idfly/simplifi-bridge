ba9feb932c169ca056ae10beec88eaa9a30f1a67
v0ld <culet.devel@gmail.com>
2021-02-28 17:54:29 +0700

    dev bridge #2

--- adapter/adapterdgu/src/index.js
+++ adapter/adapterdgu/src/index.js
@@ -38,34 +38,24 @@ app.use(bodyParser.json());
  *  Получаем запрос, Осуществляем контроль. Подписание. Отдаем chainlink node to job.
  */
 app.post('/control', async function (req, res) {
-    console.log('REQUEST /control', req.body);
+ try{
+        //todo for get
+        console.log('REQUEST /control', req.body);
 
-    //TODO enhancment
-    let for_approve = await worker.web3.eth.getTransactionReceipt(req.body.meta.initiator.transactionHash);
-    if(req.body.meta.initiator.blockHash != for_approve.blockHash) throw Error('TODO');
-    if(for_approve.status != true || for_approve.status != '0x1') throw Error('TODO');
-
-    let data           = '0x' + req.body.data.selector;
-    let reqId          = req.body.data.dataPrefix.substring(0, 66);
-    let tx             = req.body.meta.initiator.transactionHash;
-    let receive_side   = '0x' + req.body.data.receive_side;
-    console.log(reqId);console.log(data);console.log(tx);console.log(receive_side);
-    const hashMessage  = worker.web3.utils.soliditySha3(reqId, data, tx, receive_side);
-
-    let sign = await worker.web3.eth.sign(hashMessage, ownerAdapter);
-    //https://medium.com/@yaoshiang/ethereums-ecrecover-openzeppelin-s-ecdsa-and-web3-s-sign-8ff8d16595e1
-    const v1 = '0x' + sign.slice(128+2, 130+2);
-    sign = v1 == '0x00' ? sign.substring(0, sign.length - 2) + '1b' : v1 == '0x01' ? sign.substring(0, sign.length - 2) + '1c' : sign;
+        let responseData = null;
+        // for staticcall
+        if(req.body.data.request_type === 'setResponse')  responseData =  await SetTypeControlRes(req);
+        if(req.body.data.request_type === 'get')          responseData =  req.body;
+        // for call
+        if(req.body.data.request_type === 'setRequest')  responseData =  await SetTypeControlReq(req);
 
 
-    let responseData = req.body;
-    responseData.data.sign = sign;
-    responseData.data.hashMessage = hashMessage;
-    
+        console.log('RESPONSE /post ', responseData);
 
+        
+        res.status(200).send(responseData);
 
-    console.log('RESPONSE /control ', responseData);
-    res.status(200).send(responseData);
+    }catch(e){ console.log(e); res.status(500).send({});}    
 });
 
 
@@ -73,31 +63,117 @@ app.post('/control', async function (req, res) {
  *  Получаем запрос и ходим в другую сеть 
  */
 app.post('/post', async function (req, res) {
-    console.log('REQUEST /post', req.body);
+    try{
+        console.log('REQUEST /post', req.body);
 
-    let responseData = null;
-    let data  = '0x'+req.body.data.selector;
-    // for staticcall
-    if(req.body.data.request_type === 'get')  responseData =  await GetType(data, req.body.id);
-    // for call
-    if(req.body.data.request_type === 'set')  responseData =  await SetType(req.body.data.dataPrefix.substring(0, 66),
-                                                                            req.body.data.sign, 
-                                                                            data,
-                                                                            req.body.meta.initiator.transactionHash, 
-                                                                            req.body.data.receive_side);
+        let responseData = null;
+        let data  = '0x'+req.body.data.selector;
+        // for staticcall
+        if(req.body.data.request_type === 'get')  responseData =  await GetType(data, req.body.id);
+        // for call
+        if(req.body.data.request_type === 'setRequest')  responseData =  await SetTypeAuditorRequest(req.body.data.dataPrefix.substring(2, 66),
+                                                                                                     req.body.data.sign, 
+                                                                                                     data,
+                                                                                                     req.body.meta.initiator.transactionHash, 
+                                                                                                     req.body.data.receive_side);
+
+        if(req.body.data.request_type === 'setResponse')  responseData =  await SetTypeAuditorResponse('0x' + req.body.data.correlationId,
+                                                                                                       req.body.data.sign, 
+                                                                                                       req.body.meta.initiator.transactionHash,
+                                                                                                       req.body.data.dataPrefix.substring(0, 66));
 
 
-    console.log('RESPONSE /post ', responseData);
-    res.status(200).send(responseData);
+        console.log('RESPONSE /post ', responseData);
+        res.status(200).send(responseData);
+
+     }catch(e){ console.log(e); res.status(500).send({});}
 });
 
-
-async function SetType(id, sign, data, _tx, adr_receiver){
+async function SetTypeControlRes(req){
     try{
-        console.log('nonce check 1: ', await worker.web3.eth.getTransactionCount(ownerAdapter));
-        const tx  = await bridge.methods.receiveRequest(id, sign, data, _tx, '0x' + adr_receiver).send({from: ownerAdapter});
-        console.log('nonce check 2: ', await worker.web3.eth.getTransactionCount(ownerAdapter));
+            let for_approve = await worker.web3.eth.getTransactionReceipt(req.body.meta.initiator.transactionHash);
+            if(req.body.meta.initiator.blockHash != for_approve.blockHash) throw Error('TODO');
+            if(for_approve.status != true || for_approve.status != '0x1') throw Error('TODO');
 
+            let correlationId  = '0x'+req.body.data.correlationId;
+            let reqId          = req.body.data.dataPrefix.substring(0, 66);
+            let tx             = req.body.meta.initiator.transactionHash;
+            const hashMessage  = worker.web3.utils.soliditySha3(correlationId, tx, reqId);
+            let sign = await worker.web3.eth.sign(hashMessage, ownerAdapter);
+
+            //https://medium.com/@yaoshiang/ethereums-ecrecover-openzeppelin-s-ecdsa-and-web3-s-sign-8ff8d16595e1
+            const v1 = '0x' + sign.slice(128+2, 130+2);
+            sign = v1 == '0x00' ? sign.substring(0, sign.length - 2) + '1b' : v1 == '0x01' ? sign.substring(0, sign.length - 2) + '1c' : sign;
+
+
+            let responseData = req.body;
+            responseData.data.sign = sign;
+            responseData.data.hashMessage = hashMessage;
+
+            return responseData;
+
+        }catch(e){ console.log(e); throw Error('some kind error');}            
+
+}
+
+async function SetTypeControlReq(req){
+try{
+        //TODO enhancment
+        let for_approve = await worker.web3.eth.getTransactionReceipt(req.body.meta.initiator.transactionHash);
+        if(req.body.meta.initiator.blockHash != for_approve.blockHash) throw Error('TODO');
+        if(for_approve.status != true || for_approve.status != '0x1') throw Error('TODO');
+
+        let data           = '0x' + req.body.data.selector;
+        let reqId          = req.body.data.dataPrefix.substring(2, 66);
+        let tx             = req.body.meta.initiator.transactionHash;
+        let receive_side   = '0x' + req.body.data.receive_side;
+        const hashMessage  = worker.web3.utils.soliditySha3(reqId, data, tx, receive_side);
+
+        let sign = await worker.web3.eth.sign(hashMessage, ownerAdapter);
+        //https://medium.com/@yaoshiang/ethereums-ecrecover-openzeppelin-s-ecdsa-and-web3-s-sign-8ff8d16595e1
+        const v1 = '0x' + sign.slice(128+2, 130+2);
+        sign = v1 == '0x00' ? sign.substring(0, sign.length - 2) + '1b' : v1 == '0x01' ? sign.substring(0, sign.length - 2) + '1c' : sign;
+
+
+        let responseData = req.body;
+        responseData.data.sign = sign;
+        responseData.data.hashMessage = hashMessage;
+
+        return responseData;
+
+    }catch(e){ console.log(e); throw Error('some kind error');}
+}
+
+async function SetTypeAuditorResponse(correlationId, sign, _tx, reqId){
+    try{
+
+            const tx  = await bridge.methods.receiveResponse(correlationId, sign, _tx, reqId).send({from: ownerAdapter});
+            //TODO negative variant
+            let responseData = {};
+
+            while(true){
+                console.log(`>>>>>>>>>>>>>>> TX: ${tx.transactionHash}, WAITING RECEIPT`);
+                await worker.timeout(10000);
+
+                let receipt = await worker.web3.eth.getTransactionReceipt(tx.transactionHash);
+                if (receipt != null){
+                    
+                    if(receipt.status == false) throw Error({error: 1, status_text:'tx status false'});
+                    //responseData.jobRunID = id; now this does not require because should't be response to chainlink node
+                    responseData.data     = {result: tx.transactionHash, tx: receipt };
+
+                    break;
+                }
+            }
+
+            return responseData;
+
+        }catch(e){console.log(e); throw Error(e.message);}    
+}
+
+async function SetTypeAuditorRequest(id, sign, data, _tx, adr_receiver){
+    try{
+        const tx  = await bridge.methods.receiveRequest(id, sign, data, _tx, '0x' + adr_receiver).send({from: ownerAdapter, gas: 500000});
         //TODO negative variant
         let responseData = {};
 
@@ -106,29 +182,31 @@ async function SetType(id, sign, data, _tx, adr_receiver){
             await worker.timeout(10000);
 
             let receipt = await worker.web3.eth.getTransactionReceipt(tx.transactionHash);
-            if (receipt != null){ //TODO receipt.status === true
-                //console.log('receipt: ',receipt);
+            if (receipt != null){ 
                 
-                responseData.jobRunID = id;
+                if(receipt.status == false) throw Error({error: 1, status_text:'tx status false'});
+                //responseData.jobRunID = id; now this does not require because should't be response to chainlink node
                 responseData.data     = {result: tx.transactionHash, tx: receipt };
 
                 break;
             }
         }
-console.log('nonce check 3: ', await worker.web3.eth.getTransactionCount(ownerAdapter));
-        return responseData
 
-    }catch(e){console.log(e);}    
+        return responseData;
+
+    }catch(e){console.log(e); throw Error(e.message);}    
 }
 
 async function GetType(data, id){
+try{
+        let response = await bridge.methods.lowLevelGet(data).call();
+        let responseData          = {};
+            responseData.jobRunID = id;
+            responseData.data     = {result: response};
 
-    let response = await bridge.methods.lowLevelGet(data).call();
-    let responseData          = {};
-        responseData.jobRunID = id;
-        responseData.data     = {result: response};
+        return responseData;    
 
-    return responseData;    
+    }catch(e){console.log(e); throw Error('some kind error');}    
 }
 
 app.get('/ping', async function (req, res) {
